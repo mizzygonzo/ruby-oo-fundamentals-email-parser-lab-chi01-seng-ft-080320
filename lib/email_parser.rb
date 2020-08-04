735a12fce266837384da6858a6a8d52c6629abae
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-08-04 21:07:15 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,9 @@ class EmailAddressParser
    end
     
   def parse
-     @email.split(/[,\s]+/)
+     email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
  end
     
